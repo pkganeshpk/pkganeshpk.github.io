650ae27b63376b8aa8420276286d8d825d5aa3af
pkganeshpk <pkganeshpk@gmail.com>
2016-08-04 15:17:29 -0700

    Added figs to Comm

--- comm-sim/comm-sim.php
+++ comm-sim/comm-sim.php
@@ -1,31 +1,78 @@
 <section> <!--- comm sim -->
 
+	<section>
+					<h3>Agenda</h3>
+					
+						<table> 
+						<tr class = "noBorder">
+			
+						<td class = "" 	="1" style="width:1000px;border:0px">	
+					 	<figure>
+						<img src="Figs/sideview_white.jpg"  height="100" width="225"/>	
+					   	<figcaption> <normal> <b style="color:green"> CT in Ants   </b> </normal></figcaption>
+						</figure>
+						</td>
+
+						<td class = "" 	="2"  style="border:0px">	
+					 	<figure>
+						<img src="Figs/RoboZyme_thinks.png"  height="100" width="225"/>	
+					   	<figcaption> <normal> <b style="color:green"> Multi-Boundary Coverage <p>  </p>  </b> </normal></figcaption>
+						</figure>
+						</td>
+					
+						</tr>
+
+						<tr class = "noBorder">
+						<td class = "" 	="3" style="width:1000px;border:0px">	
+					 	<figure>
+						<img src="Figs/config.svg"  height="100" width="225"/>	
+					   	<figcaption> <normal>  <b style="color:green"> Analyzing $\mathscr{G}$ <p>  </p>  </b> </normal></figcaption>
+						</figure>
+						</td>
+
+						<td class = "" 	="4" style="width:1000px;border:0px">	
+					 	<figure>
+						<img src="Figs/Pheeno1.jpg"  height="100" width="225"/>	
+					   	<figcaption> <normal>  <b style>Experiments <p>  </p>  </b> </normal></figcaption>
+						</figure>
+						</td>	
+
+						</table>
+				 
+						
+					</section>
+
 
 <section>
 <h3> Towards Realistic MultiRobot + Wireless Simulation</h3>
 
-<img src="comm-sim/Figs/roslogo.png"  height="90" width="200" style="position:absolute; TOP:350px; LEFT:-250px;"/>
+<img src="comm-sim/Figs/ros-big.png"  height="90" width="200" style="position:absolute; TOP:350px; LEFT:-250px; border:0px"/>
 
-<img src="comm-sim/Figs/swarmanoid.jpeg"  height="200" width="350" style="position:absolute; TOP:350px; RIGHT:-250px;"/>
-<img src="comm-sim/Figs/geni-logo.png"  height="90" width="150" style="position:absolute; TOP:350px; RIGHT:450px;"/>		
+<img src="comm-sim/Figs/swarmanoid.jpeg"  height="200" width="350" style="position:absolute; TOP:350px; RIGHT:-250px; border:0px"/>
+<img src="comm-sim/Figs/geni-logo.png"  height="90" width="150" style="position:absolute; TOP:-50px; RIGHT:-250px; border:0px"/>		
 		
 </section>
 
 <section>
 <h3> ROS </h3>
 
-<table>
-<tr class="noBorder" width="1200px">
-<table>
-<tr class="noBorder" width="1200px">
-<td>
-<img src="comm-sim/Figs/ros2.png"  height="200" width="300" class="fragment" data-fragment-index="1"/>	
-</td>
+<img src="comm-sim/Figs/Ros-Turtle.jpg"  height="100" width="200" style="position:absolute; TOP:-50px; RIGHT:-250px; border:0px" />	
 
-<td "vertical-align:top;">
-<ul>
-<li class="fragment" 	data-fragment-index="1"> Excellent implementation of pose tracking, motion planning, etc.
-<li class="fragment" 	data-fragment-index="2"> <b> Limited </b> Multi-Robot simulation support. 
+<table>
+<tr class="noBorder" width="1200px">
+<table>
+<tr class="noBorder" style="border:0px;">
+<td class="noBorder" style="border:0px;">
+<img src="comm-sim/Figs/ros2.png"  height="300" width="500"/>	
+</td>
+</tr>
+
+<tr >
+<td "vertical-align:top;" style="border:0px;">
+
+<ul style="list-style: none;">
+<li> &#x2714; Excellent implementation of pose tracking, motion planning, etc.
+<li> &#x2718; Limited </b> Multi-Robot simulation support. 
 </ul>
 
 </td>
@@ -39,29 +86,28 @@
 
 <h3> ARGoS  </h3>
 
-<table>
-<tr class="noBorder" width="1200px">
-<td>
-<img src="comm-sim/Figs/argos2.jpeg"  height="200" width="300" class="fragment" data-fragment-index="1"/>	
-</td>
 
+
+<img src="comm-sim/Figs/argos2.jpeg"  height="200" width="300" />	
+
+<table>
+<tr>
+<td>
 <td style="vertical-align:top;">
 
 
-<ul>
 
-<li class="fragment" 	data-fragment-index="1"> <b> Excellent </b> Multi-Robot simulation
-<ul>
-<li class="fragment" 	data-fragment-index="1"> Up to 100,000 Robots supported <a href="http://goo.gl/Eswuj1">(Pinciroli et al., 2011) </a>
-</ul> 
-<li class="fragment" 	data-fragment-index="2">Little pose tracking, motion planning, etc.
+<ul style="list-style: none;">
+<li> &#x2714; <normal  style="display:inline"> <b> Excellent </b> Multi-Robot simulation of up to 100K Robots  <a href="http://goo.gl/Eswuj1"> </normal> <small style="display:inline"> Pinciroli et al., 2011 </small> </a>
+ 
+<li> &#x2718; Little pose tracking, motion planning, etc.
 
-<li class="fragment" 	data-fragment-index="3"> RoboNetsim patches ARGoS-2 with NS-3 <a href="http://goo.gl/CIcR75"> (Kudelski et al., 2013) </a>
 
- <ul>
-<li class="fragment" 	data-fragment-index="3"> <b> Does not work with ARGoS3! </b>
-</ul> 
-</ul>
+<li> &#x2718; RoboNetsim patch  <a href="http://goo.gl/CIcR75"> <small> Kudelski et al., 2013 </small> </a> doesn't work with ARGoS3 
+
+
+
+
 </td>
 
 
@@ -73,18 +119,18 @@
 <h3> <a href="https://www.geni.net/?page_id=2"> GENI </a>  </h3>
 
 <table>
-<tr class="noBorder" width="1200px">
+<tr class="noBorder" style="width:800px;">
 <td>
-<img src="comm-sim/Figs/Geni-manet.jpg"  height="300" width="300" class="fragment" data-fragment-index="1"/>	
+<img src="comm-sim/Figs/Geni-manet.jpg"  height="300" width="500" />	
 </td>
 
 <td style="vertical-align:top;">
 
 <ul>
-<li class="fragment" 	data-fragment-index="1"> Virtual networking lab, with support for MANET
-<li class="fragment" 	data-fragment-index="2"> Can be used for accurate Wi-fi experiments
-<li class="fragment" 	data-fragment-index="3"> Precludes need for simulation
-<li class="fragment" 	data-fragment-index="4"> Not obvious how to combine this with robots 
+<li class="" 	="1"> Network-as-a-Service, with MANET Support
+<li class="" 	="2"> Can be used for accurate Wi-fi experiments
+<li class="" 	="3"> Precludes need for simulation
+<li class="" 	="4"> Not obvious how to combine this with robots 
  
 </ul>
 </tr>
